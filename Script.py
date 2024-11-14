f3d158776d639f9ce1c94959dc945335e3732aca
StevenS07 <owlq07@gmail.com>
2024-11-14 12:04:31 +0100

    Remove api key

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 #Add key
-OpenAI_API_KEY = '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'
+OpenAI_API_KEY = ''
 client = OpenAI(api_key=OpenAI_API_KEY)
 #Read files to edit
 with open('Files/artykul.txt', 'r', encoding='utf-8') as file:
